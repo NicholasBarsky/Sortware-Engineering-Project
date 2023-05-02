7094d31bb55475876724106da1fea6e260756dc6
Brandon Catalano <103974491+brandjtc@users.noreply.github.com>
2023-05-02 17:26:54 -0400

    Update quiz creator.sql

--- sql builder files/quiz creator.sql
+++ sql builder files/quiz creator.sql
@@ -24,22 +24,6 @@ values('What type of die is used to do skill checks?','36 sided','18 sided','10 
 ('Which campaign book is based off the Netflix Series Stranger Things?','Out of the Abyss','Guide to the Upside Down','Demogorgon Hunters','Hunt for the Thessalhydra','Hunt for the Thessalhydra is a Netflix inspired campaign book which can be seen on the Campaign books page.'),
 ('Which class is known for shapeshifting into various animals?','Sorcerer','Warlock','Shifter','Druid','Druids are a class closely attuned with nature, including animals.'),
 ('What is D&D?','A popular RPG videogame set in a Fantasy World.','A Fantasy Boardgame','A Fantasy Movie Franchise','The Most Popular Tabletop RolePlaying Game in the World!','This one should be somewhat obvious, but D&D is the most popular tabletop roleplaying game in the world.'),
-('What sided dice is used to roll for initiative?','12 sided die','6 sided die','36 sided die','20 sided die','20 sided die are used to roll for initiative.'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video'),
-('Whos head is in the toilet bowl?','Brandon','Dr. Kumar','Nic','Gumuchian','from music video');
+('What sided dice is used to roll for initiative?','12 sided die','6 sided die','36 sided die','20 sided die','20 sided die are used to roll for initiative.');
 --@block
 Select * from questionformat;
